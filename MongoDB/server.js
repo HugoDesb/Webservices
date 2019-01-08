65e36967a556d23e48e0858d649f889380d9ec6c
Hugo <hugo.desbiolles@gmail.com>
2019-01-08 15:33:11 +0100

    lab uni and doc

--- MongoDB/server.js
+++ MongoDB/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/search/lab', function(req, res){
-    dataLabLayer.searchLab(req.body.query, function(publicationSet){
+    dataLabLayer.getLab(req.body.query, function(publicationSet){
         res.send(publicationSet);
     })
 });
